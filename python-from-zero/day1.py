e68454bc0aadf51ea1edadad7eec18d3018e32d9
rahadyaN <rahadyanarkana@gmail.com>
2026-01-05 14:00:37 +0700

    update: update code

--- python-from-zero/day1.py
+++ python-from-zero/day1.py
@@ -1,11 +1,23 @@
-print("Helo, World!")
+# python rentang terhadap spasi atau indentation
+print("Helo, World!") # statement / perintah
 
-a = 3
-b = 5
+# variabel = sebuah tempat untuk menaruh data
+# Data type : python sudah dinamis
+nama = "susi" # string
+umur = 54 # integer
+berat_badan = 78.5 # floating point
+jomblo = True
 
-print(a+b)
+print("nama saya:", nama)
+print("umur saya:", umur)
 
-if a == 4:
-    print("True")
-else :
-    print("False")
+# penamaan variabel 
+# valid = berat_badan, pakai underscore _ ,
+# ga valid = berat badan
+
+# ubah tipe data
+umur = float(19)
+berat_badan = int(78.5)
+
+print(type(umur))
+print(type(berat_badan))
